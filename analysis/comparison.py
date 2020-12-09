cdf9991e7175d353a75dea66b9ad5e548e12291d
Ahmed Shariff <ahmed.shariff@rocketmail.com>
2020-12-09 03:03:49 -0600

    calculating metrics

--- analysis/comparison.py
+++ analysis/comparison.py
@@ -2,54 +2,108 @@ import numpy as np
 import json
 from pathlib import Path
 from sklearn.neighbors import BallTree
+from sklearn.metrics import dcg_score
 from easydict import EasyDict
 import cv2
+from mlpipeline import MetricContainer, iterator
+from mlpipeline.utils import set_logger
+from tqdm import tqdm
+from repcomp.comparison import CCAComparison, NeighborsComparison
+
+set_logger()
+
+export_dirs = [
+    "conv-flat",
+    # "conv-non-flat",
+    "linear-flat",
+    "linear-non-flat"]
 
 def main():
-    export_roots = list(Path("../exports").glob("*flat"))
-    ui_layout_vector = get_ui_layout()
-
-    for root_dir in (export_roots[3], ):
+    export_roots = [Path("../exports") / d for d in export_dirs]
+    
+    for root_dir in export_roots:  # (export_roots[2], ):
         train_data = get_trained_data(root_dir)
-        for name, idx in list(train_data.name_to_idx.items())[:10]:
-            train_d, train_i = train_data.tree.query([train_data.vectors[idx]], k=10)
+        ui_layout_vector = get_ui_layout(train_data)
+        mc = MetricContainer(["precision", "recall", "dcg", "skipped"])
+        # comparator = CCAComparison()
+        # sim = comparator.run_comparison(train_data.vectors[:50], ui_layout_vector.vectors[:50])
+        # print(sim)
+        for name, idx in tqdm(iterator(train_data.name_to_idx.items(), None), total=len(train_data.name_to_idx)):
+            train_d, train_i = train_data.tree.query([train_data.vectors[idx]], k=20)
             train_results = [train_data.idx_to_name[i] for i in train_i[0]]
+
+            try:
+                ui_idx = ui_layout_vector.name_to_idx[name]
+                ui_d, ui_i= ui_layout_vector.tree.query([ui_layout_vector.vectors[ui_idx]], k=20)
+                ui_results = [ui_layout_vector.idx_to_name[i] for i in ui_i[0]]
+            except KeyError:
+                mc.skipped.update(1)
+                continue
+            mc.skipped.update(0)
+            # print("ui")
+            # _display_images(name, *ui_results)
+            # print("train")
+            # _display_images(name, *train_results)
             
-            ui_idx = ui_layout_vector.name_to_idx[name]
-            ui_d, ui_i= ui_layout_vector.tree.query([ui_layout_vector.vectors[ui_idx]], k=10)
-            ui_results = [ui_layout_vector.idx_to_name[i] for i in ui_i[0]]
+            # print(train_results)
+            # print(ui_results)
 
-            print("ui")
-            _display_images(name, *ui_results)
-            print("train")
-            _display_images(name, *train_results)
+            mc.precision.update(_precision(ui_results, train_results))
+            mc.recall.update(_recall(ui_results, train_results))
+            mc.dcg.update(_dcg(ui_results, train_results))
 
-            print(train_results)
-            print(ui_results)
         #     break
         # break
+        mc.log_metrics()
     
     # for name, idx in ui_layout_vector.name_to_idx:
     #     print(name, idx)
     #     break
 
-    
-def get_ui_layout():
+
+def _precision(ui_results, train_results):
+    return len(set(train_results).intersection(ui_results))/len(train_results)
+
+
+def _recall(ui_results, train_results):
+    return len(set(train_results).intersection(ui_results))/len(ui_results)
+
+
+def _dcg(ui_results, train_results):
+    lables = set(ui_results)
+    lables.update(train_results)
+    true_labels = [[1 if l in ui_results else 0 for l in lables]]
+    pred_labels = [[1 if l in train_results else 0 for l in lables]]
+    return dcg_score(true_labels, pred_labels)
+
+
+def get_ui_layout(train_data):
     data_directory = Path("../data")
 
     ui_layout_vectors_path =  data_directory / "ui_layout_vectors"
     
     with open(ui_layout_vectors_path / "ui_names.json") as f:
-        ui_layout_vector_names = [n.rstrip(".png") for n in json.load(f)["ui_names"]]
-        ui_layout_vector_name_to_idx = zip(ui_layout_vector_names, range(len(ui_layout_vector_names)))
-        ui_layout_vector_idx_to_name = zip(range(len(ui_layout_vector_names)), ui_layout_vector_names)
+        names = [n.rstrip(".png") for n in json.load(f)["ui_names"]]
+        name_to_idx = zip(names, range(len(names)))
+        idx_to_name = zip(range(len(names)), names)
 
-    ui_layout_vectors = np.load(ui_layout_vectors_path / "ui_vectors.npy")
+    vectors = np.load(ui_layout_vectors_path / "ui_vectors.npy")
 
-    print(ui_layout_vectors.shape)
-    ui_layout_vector_tree = BallTree(ui_layout_vectors)
+    filtered_name_to_idx = []
+    filtered_idx_to_name = []
+    filtered_vectors = []
+    for idx in range(len(train_data.vectors)):
+        name = train_data.idx_to_name[idx]
+        
+        filtered_name_to_idx.append((name, idx))
+        filtered_idx_to_name.append((idx, name))
+        filtered_vectors.append(vectors[idx])
+
+    filtered_vectors = np.array(filtered_vectors)
+    # print(filtered_vectors.shape)
+    tree = BallTree(filtered_vectors)
     
-    return Data(ui_layout_vector_tree, ui_layout_vector_name_to_idx, ui_layout_vector_idx_to_name, ui_layout_vectors)
+    return Data(tree, filtered_name_to_idx, filtered_idx_to_name, filtered_vectors)
 
 
 def _strip_npy_name(name):
@@ -79,7 +133,7 @@ def get_trained_data(root_dir):
     encoding_vectors = np.array(encoding_vectors)
     print(idx, encoding_vectors.shape, root_dir)
     tree = BallTree(encoding_vectors)
-    print(tree.query(encoding_vectors[:2]))
+    # print(tree.query(encoding_vectors[:2]))
 
     return Data(tree, name_to_idx, idx_to_name, encoding_vectors, train_names, test_names)
 
